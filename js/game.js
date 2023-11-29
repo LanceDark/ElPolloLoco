e3229b413fd03fa340f5706f705fb4eaa5ed43fb
LanceDark <dominik.maeder.97@web.de>
2023-11-29 15:24:04 +0100

    added few buttons for Menu

--- js/game.js
+++ js/game.js
@@ -19,51 +19,45 @@ function startScreen() {
     let height = Math.min(maxHeight, img.height);
 
     ctx.drawImage(img, 0, 0, width, height);
-    addStartButtonDraw(canvas, ctx);
   };
 }
 
-/**
- * startingButton added which is starting init (means the full Game starts on this click)
- * @param {*} canvas
- * @param {*} ctx
- */
-
-function addStartButtonDraw(canvas, ctx) {
-  designForButton(ctx);
-  canvas.addEventListener("click", function (event) {
-    let rect = canvas.getBoundingClientRect();
-    let mouseX = event.clientX - rect.left;
-    let mouseY = event.clientY - rect.top;
-
-    if (mouseX >= 300 && mouseX <= 450 && mouseY >= 50 && mouseY <= 100) {
-      init();
-    }
-  });
-}
-
-/**
- * design for the Button
- * @param {*} ctx
- */
-
-function designForButton(ctx) {
-  ctx.fillStyle = "orange";
-  ctx.fillRect(300, 50, 150, 50);
-
-  ctx.strokeStyle = "black";
-  ctx.lineWidth = 3;
-  ctx.strokeRect(300, 50, 150, 50);
-
-  ctx.fillStyle = "black"; // color
-  ctx.font = "20px Arial"; // font
-  ctx.fillText("Spiel starten", 318, 83);
-}
 
 function init() {
   canvas = document.getElementById("playground");
   world = new World(canvas, keyboard);
   console.log("My Character is", world.character);
+  deleteButton();
+}
+
+function  deleteButton(){
+  let menuButtons = document.getElementById("menu")
+  menuButtons.style.display = "none"
+}
+
+function openSettings(){
+  getNewBackground();
+}
+
+function getNewBackground(){
+  let canvas = document.getElementById("playground");
+  let ctx = canvas.getContext("2d");
+  let img = new Image();
+  img.src = "./img/polloBG.jpg";
+
+  img.onload = function () {
+    let maxWidth = 720;
+    let maxHeight = 480;
+
+    let width = Math.min(maxWidth, img.width);
+    let height = Math.min(maxHeight, img.height);
+
+    ctx.drawImage(img, 0, 0, width, height);
+  };
+}
+
+function pepeStory(){
+  getNewBackground();
 }
 
 window.addEventListener("keydown", (e) => {
@@ -108,19 +102,3 @@ window.addEventListener("keyup", (e) => {
   }
 });
 
-function gameOverScreen() {
-  let canvas = document.getElementById("playground");
-  let ctx = canvas.getContext("2d");
-  let img = new Image();
-  img.src = "./img/9_intro_outro_screens/game_over/oh no you lost!.png";
-
-  img.onload = function () {
-    let maxWidth = 720;
-    let maxHeight = 480;
-
-    let width = Math.min(maxWidth, img.width);
-    let height = Math.min(maxHeight, img.height);
-
-    ctx.drawImage(img, 0, 0, width, height);
-  };
-}
